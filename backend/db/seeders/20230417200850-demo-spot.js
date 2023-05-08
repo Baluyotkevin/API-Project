a3cd197c0c825a06610326c351dfcf33e1a881c3
Kevin Baluyot <baluyotkevin33@yahoo.com>
2023-05-08 12:39:13 -0700

    changed the seeder ownerspots

--- backend/db/seeders/20230417200850-demo-spot.js
+++ backend/db/seeders/20230417200850-demo-spot.js
@@ -13,38 +13,38 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         ownerId: 1,
-        address: 'here',
-        city: 'me',
-        state: 'yellow',
-        country: 'everywhere',
+        address: '1215 Hobbit Street',
+        city: 'Hobbity-city',
+        state: 'Hobbit',
+        country: 'Middle-earth',
         lat: 23.2,
         lng: 33.3,
-        name: 'mynamew',
-        description: 'allover',
-        price: 333.23
+        name: 'Hobbit-house',
+        description: 'One and only hobbit house to stay at!',
+        price: 99999.99
       },
       {
         ownerId: 2,
-        address: 'your heart baby',
-        city: 'you tell me',
-        state: 'wherever you want to go',
-        country: 'next to your heart',
+        address: 'Somewhere in Gotham',
+        city: 'Gotham City',
+        state: 'Some state',
+        country: 'Some country',
         lat: 0.1,
         lng: 0.12,
-        name: "yes",
-        description: 'beautiful',
+        name: "Batman's House",
+        description: "You get to see all of his stuff and ride his batmobile",
         price: 2
       },
       {
         ownerId: 1,
         address: 'wherever you want it to be',
-        city: 'you',
-        state: 'red',
-        country: 'everywhere you go',
+        city: 'Pallet Town',
+        state: 'Somewhere',
+        country: 'Kanto',
         lat: 23.2,
         lng: 33.3,
-        name: 'thespot',
-        description: 'thebestspot',
+        name: "Ash Ketchum's house",
+        description: "You not only get to stay in Ash Ketchum's house, but you also get to hang out with Pikachu",
         price: 999.99
       },
       {
@@ -56,19 +56,19 @@ module.exports = {
         lat: 0.1,
         lng: 0.12,
         name: "yes",
-        description: 'beautifulspot',
+        description: 'This is a beautiful home that you will enjoy. I promise',
         price: 5923
       },
       {
         ownerId: 3,
-        address: 'beats me',
-        city: 'yourcity',
-        state: 'nostate',
-        country: 'somewhere south',
+        address: 'Kirbys home',
+        city: 'Kirbys city',
+        state: 'Kirbys state',
+        country: 'Kirbys country',
         lat: 0.1,
         lng: 0.12,
-        name: "BEAUTIFULAMAZINGSPOT",
-        description: 'youcanalreadyguessthatsbeautiful',
+        name: "Kirby's home",
+        description: 'You get to see what Kirby does on a daily basis.',
         price: 5923
       }
 
